620599bfdb99e99130380b53bd1d5d1db7d9edb6
sakhilsaiteaj <sakhilsaiteaj@gmail.com>
2025-01-29 10:23:55 +0530

    net

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import { ChatProvider } from "./hooks/useChat";
 import { AvatarProvider } from "./hooks/useAvatar";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChatProvider>
       <AvatarProvider>
-        <App />
+          <BrowserRouter>
+              <App />
+            </BrowserRouter>
       </AvatarProvider>
     </ChatProvider>
-  </React.StrictMode>
+	</React.StrictMode>
 );
