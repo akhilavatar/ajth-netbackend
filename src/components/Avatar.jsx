5080c6241a5c3a3fe60169a05595a145ecc51a96
sakhilsaiteaj <sakhilsaiteaj@gmail.com>
2025-01-29 18:32:46 +0530

    net

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -10,7 +10,7 @@ import { filterEndTracks } from "../utils/animations";
 import { applyMorphTarget } from "../utils/morphTargets";
 
 export function Avatar(props) {
-  const { nodes, materials, scene } = useGLTF("/models/64f1a714fe61576b46f27ca2.glb");
+  const { nodes, materials, scene } = useGLTF("/models/678250d39d91f0cde79136f6.glb");
   const { animations: originalAnimations } = useGLTF("/models/animations.glb");
   const animations = filterEndTracks(originalAnimations);
   const group = useRef();
@@ -117,34 +117,24 @@ export function Avatar(props) {
             morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
           />
           <skinnedMesh
-            name="Wolf3D_Body"
+            geometry={nodes.Wolf3D_Hair.geometry}
+            material={materials.Wolf3D_Hair}
+            skeleton={nodes.Wolf3D_Hair.skeleton}
+          />
+          <skinnedMesh
             geometry={nodes.Wolf3D_Body.geometry}
             material={materials.Wolf3D_Body}
             skeleton={nodes.Wolf3D_Body.skeleton}
           />
           <skinnedMesh
-            name="Wolf3D_Outfit_Bottom"
-            geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-            material={materials.Wolf3D_Outfit_Bottom}
-            skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-          />
-          <skinnedMesh
-            name="Wolf3D_Outfit_Footwear"
             geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
             material={materials.Wolf3D_Outfit_Footwear}
             skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
           />
           <skinnedMesh
-            name="Wolf3D_Outfit_Top"
             geometry={nodes.Wolf3D_Outfit_Top.geometry}
             material={materials.Wolf3D_Outfit_Top}
             skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-          />
-          <skinnedMesh
-            name="Wolf3D_Hair"
-            geometry={nodes.Wolf3D_Hair.geometry}
-            material={materials.Wolf3D_Hair}
-            skeleton={nodes.Wolf3D_Hair.skeleton}
           />
         </group>
       </group>
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import { ChatProvider } from "./hooks/useChat";
 import { AvatarProvider } from "./hooks/useAvatar";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChatProvider>
       <AvatarProvider>
-          <BrowserRouter>
-              <App />
-            </BrowserRouter>
+        <App />
       </AvatarProvider>
     </ChatProvider>
-	</React.StrictMode>
+  </React.StrictMode>
 );
