6d14603db6555b671cbbaad3cea5ca65656bc041
sakhilsaiteaj <sakhilsaiteaj@gmail.com>
2025-02-10 21:51:14 +0530

    net speech added backend

--- frontend/src/utils/morphTargets.js
+++
@@ -1,8 +0,0 @@
-export const applyMorphTarget = (node, targetName, value) => {
-  if (!node?.morphTargetDictionary) return;
-  
-  const idx = node.morphTargetDictionary[targetName];
-  if (typeof idx !== 'undefined') {
-    node.morphTargetInfluences[idx] = value;
-  }
-};
